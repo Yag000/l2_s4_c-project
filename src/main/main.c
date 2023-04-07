ce8a905dc7a37890ff69c93c15df6740ccc2dd26
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2023-04-07 21:16:39 +0200

    Initial main

---
+++ src/main/main.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+#include "parser.h"
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        perror("Nombre d'arguments incorrect");
+        return -1;
+    }
+
+    int error_code = parse_file(argv[1]);
+
+    return error_code == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
+}
