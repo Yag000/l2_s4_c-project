7cf7d9834d88fa827068d7c30bc5fcbe4b2724c4
gdudilli <gdudillieu@gmail.com>
2023-04-08 18:34:06 +0200

    Create an header with structures

---
+++ src/main/treeDirCore.h
@@ -0,0 +1,18 @@
+#ifndef TREE_DIR_CORE_H
+#define TREE_DIR_CORE_H
+
+typedef struct noeud
+{
+    bool est_dossier ;
+    char nom [100];
+    struct noeud * pere ;
+    struct noeud * racine ;
+    struct liste_noeud * fils ;
+} noeud;
+
+typedef struct liste_noeud {
+    struct noeud * no ;
+    struct liste_noeud * succ ;
+} liste_noeud;
+
+#endif
