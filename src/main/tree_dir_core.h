154736fc92db37ca826d77d5a208691d379c95db
gdudilli <gdudillieu@gmail.com>
2023-04-09 18:04:14 +0200

    update header

--- src/main/tree_dir_core.h
+++ src/main/tree_dir_core.h
@@ -3,27 +3,33 @@
 
 #include <stdbool.h>
 
+#define MAX_LENGTH_NOM 100
+
 typedef struct noeud
 {
-    bool est_dossier ;
-    char nom [100];
-    struct noeud * pere ;
-    struct noeud * racine ;
-    struct liste_noeud * fils ;
+    bool est_dossier;
+    char nom[MAX_LENGTH_NOM];
+    struct noeud *pere;
+    struct noeud *racine;
+    struct liste_noeud *fils;
 } noeud;
 
-typedef struct liste_noeud {
-    struct noeud * no ;
-    struct liste_noeud * succ ;
+typedef struct liste_noeud
+{
+    struct noeud *no;
+    struct liste_noeud *succ;
 } liste_noeud;
 
-noeud* create_noeud();
-void destroy_noeud(noeud* node);
-bool are_equal(noeud* node1, noeud* node2);
-liste_noeud *create_liste_noeud();
-void destroy_liste_noeud(liste_noeud* node);
-bool contains_liste_noeud(noeud* node);
-bool append_liste_noeud(noeud* node);
-bool remove_liste_noeud(noeud* node);
+noeud *create_empty_noeud();
+noeud *create_noeud(bool est_dossier, char *nom, noeud *pere, noeud *racine);
+noeud *create_noeud_with_fils(bool est_dossier, char *nom, noeud *pere, noeud *racine, liste_noeud *fils);
+void destroy_noeud(noeud *node);
+bool are_noeud_equal(noeud *node1, noeud *node2);
+bool is_fils_of_node_empty(noeud *node);
+liste_noeud *create_liste_noeud(noeud *node);
+void destroy_liste_noeud(liste_noeud *node_list);
+bool contains_liste_noeud(liste_noeud *node_list, noeud *node);
+bool append_liste_noeud(liste_noeud *node_list, noeud *node);
+bool remove_liste_noeud(liste_noeud *node);
 
 #endif
