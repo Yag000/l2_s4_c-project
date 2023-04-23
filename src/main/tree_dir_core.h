37af326663bd6df792c1e5f125d0714807a04ed0
GabinDDL <gdudillieu@gmail.com>
2023-04-24 00:48:35 +0200

    add constant_node of tree_dir_core

--- src/main/tree_dir_core.h
+++ src/main/tree_dir_core.h
@@ -20,6 +20,8 @@ typedef struct liste_noeud
     struct liste_noeud *succ;
 } liste_noeud;
 
+extern noeud *constant_node;
+
 noeud *create_noeud(bool est_dossier, const char *nom, noeud *pere);
 noeud *create_noeud_with_fils(bool est_dossier, const char *nom, noeud *pere, liste_noeud *fils);
 noeud *create_root_noeud();
