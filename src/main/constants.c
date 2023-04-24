85dd4aa89b922c65d33ea11880fa2b7965ed9896
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2023-04-24 21:21:17 +0200

    Add more error constants

--- src/main/constants.c
+++ src/main/constants.c
@@ -1,4 +1,6 @@
 #include "constants.h"
 
-const int FATAL_ERROR = -1; // Error code that indicates that he program should stop
-const int INVALID_NAME = 1; // Error code that indicates that the name of a node is invalid
+const int FATAL_ERROR = -1;           // Error code that indicates that he program should stop
+const int INVALID_NAME = 1;           // Error code that indicates that the name of a node is invalid
+const int INVALID_PATH = 2;           // Error code that indicates that the path of a node is invalid
+const int INVALID_NUMBER_OF_ARGS = 3; // Error code that indicates that the number of arguments is invalid
--- src/main/constants.h
+++ src/main/constants.h
@@ -7,7 +7,9 @@
 extern FILE *out_stream;
 extern bool verbose;
 
-extern const int FATAL_ERROR;  // Error code that indicates that he program should stop
-extern const int INVALID_NAME; // Error code that indicates that the name of a node is invalid
+extern const int FATAL_ERROR;            // Error code that indicates that he program should stop
+extern const int INVALID_NAME;           // Error code that indicates that the name of a node is invalid
+extern const int INVALID_PATH;           // Error code that indicates that the path of a node is invalid
+extern const int INVALID_NUMBER_OF_ARGS; // Error code that indicates that the number of arguments is invalid
 
 #endif
