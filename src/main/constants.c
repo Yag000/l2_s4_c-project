d977092748b692b4dbe7e0b627432e031105854d
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2023-04-24 20:33:59 +0200

    Correct constant definition

---
+++ src/main/constants.c
@@ -0,0 +1,4 @@
+#include "constants.h"
+
+const int FATAL_ERROR = -1; // Error code that indicates that he program should stop
+const int INVALID_NAME = 1; // Error code that indicates that the name of a node is invalid
--- src/main/constants.h
+++ src/main/constants.h
@@ -7,4 +7,7 @@
 extern FILE *out_stream;
 extern bool verbose;
 
+extern const int FATAL_ERROR;  // Error code that indicates that he program should stop
+extern const int INVALID_NAME; // Error code that indicates that the name of a node is invalid
+
 #endif
