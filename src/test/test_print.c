9d426808642ea0664db947d0b54dd379abbfb17b
GabinDDL <gdudillieu@gmail.com>
2023-04-29 00:20:25 +0200

    Remove names in def

--- src/test/test_print.c
+++ src/test/test_print.c
@@ -10,8 +10,8 @@
 #include "../main/string_utils.h"
 
 static void test_print_function(test_info *);
-static void test_print_while_creating_tree(test_info *info);
-static void invalid_name_format_test_handler(test_info *info);
+static void test_print_while_creating_tree(test_info *);
+static void invalid_name_format_test_handler(test_info *);
 
 test_info *test_print_command()
 {
