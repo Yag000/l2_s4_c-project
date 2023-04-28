e976bb3d7d46f01c93d26713f9d03c474b038ca3
GabinDDL <gdudillieu@gmail.com>
2023-04-28 17:33:53 +0200

    update display print + code

--- src/main/print.c
+++ src/main/print.c
@@ -1,13 +1,29 @@
-#include <string.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
-#include "string_utils.h"
 #include "command.h"
+#include "string_utils.h"
 #include "tree_dir_core.h"
 
-static char *get_a_print_line(const char *nom, bool est_dossier, unsigned depth);
-static void print_with_depth(noeud *node, unsigned depth);
+#define FILLER_SIZE 4
+#define ROOT_CHAR '/'
+#define FILLER_CHAR ' '
+#define UNION_CHAR '-'
+#define BORDER_CHAR '|'
+#define TRANSITION_CHAR '\\'
+#define START_CONTOUR_CHAR '('
+#define END_CONTOUR_CHAR ')'
+#define FILE_TYPE_CHAR 'F'
+#define DIRECTORY_TYPE_CHAR 'D'
+
+static void print_with_depth(noeud *, unsigned, bool);
+static char *get_a_print_line(const char *, bool, unsigned, bool);
+static void add_start_transition_at_string(char *, unsigned, bool);
+static void add_file_name_at_string(char *, const char *, unsigned, unsigned);
+static void add_file_type_at_string(char *, unsigned, bool);
+
+static char *last_line = NULL;
 
 int print(const command *cmd)
 {
@@ -15,17 +31,23 @@ int print(const command *cmd)
     {
         return 1;
     }
-    print_with_depth(current_node->racine, 0);
+    print_with_depth(current_node->racine, 0, false);
 
+    free(last_line);
+    last_line = NULL;
     return 0;
 }
 
-static void print_with_depth(noeud *node, unsigned depth)
+static void print_with_depth(noeud *node, unsigned depth, bool is_last)
 {
-    char *line = get_a_print_line(node->nom, node->est_dossier, depth);
-    write_result_command(line);
+    char *line = get_a_print_line(node->nom, node->est_dossier, depth, is_last);
 
-    free(line);
+    write_result_command(line);
+    if (last_line != NULL)
+    {
+        free(last_line);
+    }
+    last_line = line;
 
     if (!node->est_dossier)
     {
@@ -33,50 +55,115 @@ static void print_with_depth(noeud *node, unsigned depth)
     }
     for (liste_noeud *lst = node->fils; lst != NULL; lst = lst->succ)
     {
-        print_with_depth(lst->no, depth + 1);
+        if (lst->succ == NULL)
+        {
+
+            print_with_depth(lst->no, depth + 1, true);
+        }
+        else
+        {
+            print_with_depth(lst->no, depth + 1, false);
+        }
     }
 }
 
-static char *get_a_print_line(const char *nom, bool est_dossier, unsigned depth)
+static char *get_a_print_line(const char *nom, bool est_dossier, unsigned depth, bool is_last)
 {
-    unsigned n_start_space = depth * 4;
-    unsigned n_name = strlen(nom);
+    unsigned len_start_transition = depth * FILLER_SIZE;
+    unsigned len_name = strlen(nom);
 
-    if (n_name == 0)
+    if (len_name == 0)
     {
-        n_name += 1;
+        len_name += 1;
     }
 
-    char *result = malloc(sizeof(char) * (n_start_space + n_name + 5));
+    char *result = malloc(sizeof(char) * (len_start_transition + len_name + 5));
 
-    for (unsigned i = 0; i < n_start_space; i++)
+    add_start_transition_at_string(result, len_start_transition, is_last);
+    add_file_name_at_string(result, nom, len_start_transition, len_name);
+
+    result[len_start_transition + len_name] = FILLER_CHAR;
+
+    add_file_type_at_string(result, len_start_transition + len_name + 1, est_dossier);
+
+    result[len_start_transition + len_name + 4] = '\0';
+
+    return result;
+}
+
+static void add_start_transition_at_string(char *string_to_fill, unsigned end, bool is_last)
+{
+    if (end == 0)
     {
-        result[i] = ' ';
+        return;
     }
 
-    if (strcmp(nom, "") == 0)
+    unsigned last_line_len;
+
+    if (last_line == NULL)
     {
-        result[n_start_space] = '/';
+        last_line_len = 0;
     }
     else
     {
-        memmove(result + n_start_space, nom, n_name);
+        last_line_len = strlen(last_line);
     }
 
-    result[n_start_space + n_name] = ' ';
-    result[n_start_space + n_name + 1] = '(';
+    for (unsigned i = 0; i < end - 1; i++)
+    {
+        if (i % FILLER_SIZE == 0)
+        {
+            if (is_last && end - i <= FILLER_SIZE)
+            {
+                string_to_fill[i] = TRANSITION_CHAR;
+            }
+            else if (i <= last_line_len && (last_line[i] == '\\' || last_line[i] == ' '))
+            {
+                string_to_fill[i] = FILLER_CHAR;
+            }
+            else
+            {
+                string_to_fill[i] = BORDER_CHAR;
+            }
+        }
+
+        else if (end - i < FILLER_SIZE)
+        {
+            string_to_fill[i] = UNION_CHAR;
+        }
+        else
+        {
+            string_to_fill[i] = FILLER_CHAR;
+        }
+    }
+
+    string_to_fill[end - 1] = FILLER_CHAR;
+}
+
+static void add_file_name_at_string(char *string_to_fill, const char *nom, unsigned start, unsigned len_name)
+{
+    if (strcmp(nom, "") == 0)
+    {
+        string_to_fill[start] = ROOT_CHAR;
+    }
+    else
+    {
+        memmove(string_to_fill + start, nom, len_name);
+    }
+}
+
+static void add_file_type_at_string(char *string_to_fill, unsigned start, bool est_dossier)
+{
+    string_to_fill[start] = START_CONTOUR_CHAR;
 
     if (est_dossier)
     {
-        result[n_start_space + n_name + 2] = 'D';
+        string_to_fill[start + 1] = DIRECTORY_TYPE_CHAR;
     }
     else
     {
-        result[n_start_space + n_name + 2] = 'F';
+        string_to_fill[start + 1] = FILE_TYPE_CHAR;
     }
 
-    result[n_start_space + n_name + 3] = ')';
-    result[n_start_space + n_name + 4] = '\0';
-
-    return result;
+    string_to_fill[start + 2] = END_CONTOUR_CHAR;
 }
