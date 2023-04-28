5c72b2c3f960c2620e79d4289194c8af4b9c081d
GabinDDL <gdudillieu@gmail.com>
2023-04-29 00:14:05 +0200

    Remove return at test print

--- src/test/test_print.c
+++ src/test/test_print.c
@@ -89,8 +89,6 @@ static void *test_print_while_creating_tree(test_info *info)
 
     destroy_command(cmd);
     destroy_noeud(root);
-
-    return root;
 }
 
 static void invalid_name_format_test_handler(test_info *info)
