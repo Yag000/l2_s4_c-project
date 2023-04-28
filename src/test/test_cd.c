62916bd1aca2d1498fa9eb2ba829d21c5118c465
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2023-04-28 12:57:11 +0200

    Add test structure

--- src/test/test_cd.c
+++ src/test/test_cd.c
@@ -1,9 +1,9 @@
 #include "test_core.h"
 
-#include "../main/file_manager.h"
-#include "../main/tree_dir_core.h"
 #include "../main/command.h"
 #include "../main/constants.h"
+#include "../main/file_manager.h"
+#include "../main/tree_dir_core.h"
 
 void test_go_to_root(test_info *);
 void test_go_to_previous_directory(test_info *);
@@ -20,6 +20,9 @@ test_info *test_cd()
     clock_t before = clock();
     test_info *info = create_test_info();
 
+    out_stream_path = "src/test/output/test_cd.txt";
+    out_stream = open_file(out_stream_path, "w");
+
     // Add tests here
     init_folder_struture();
     test_go_to_root(info);
@@ -28,16 +31,18 @@ test_info *test_cd()
     test_valid_paths(info);
 
     // End of tests
+    destroy_noeud(current_node->racine);
+    close_file(out_stream, out_stream_path);
+
     info->time = clock_ticks_to_seconds(clock() - before);
     print_test_footer("cd", info);
     return info;
 }
 
-void test_go_to_root(test_info *info) {
-
+void test_go_to_root(test_info *info)
+{
     print_test_name("Testing cd to root");
-    
-        
+
 }
 void test_go_to_previous_directory(test_info *info) {}
 void test_invalid_invalid_number_args(test_info *info) {}
@@ -46,14 +51,11 @@ void test_valid_paths(test_info *info) {}
 
 
 
-static void init_folder_struture(){
-
-    out_stream_path = "src/test/output/test_print.txt";
-    out_stream = open_file(out_stream_path, "w");
+static void init_folder_struture()
+{
 
     noeud *root = create_root_noeud();
     current_node = root;
-
 
     noeud *node1 = create_noeud(true, "test", root);
     append_a_fils_to_noeud(root, node1);
@@ -76,5 +78,4 @@ static void init_folder_struture(){
     append_a_fils_to_noeud(root, node1);
 
     append_a_fils_to_noeud(node1, create_noeud(false, "test12", node1));
-
 }
