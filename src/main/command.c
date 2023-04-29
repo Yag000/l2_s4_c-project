504c3522e6cef7fd2b015ab08bbacecdfdd6ca91
Dudillieu Gabin <102906650+GabinDDL@users.noreply.github.com>
2023-04-29 20:11:47 +0200

    Update src/main/command.c
    Co-authored-by: Yag000 <80483460+Yag000@users.noreply.github.com>

--- src/main/command.c
+++ src/main/command.c
@@ -157,7 +157,7 @@ bool handle_number_of_args(unsigned expected, unsigned actual)
     if (expected != actual)
     {
         fprintf(out_stream,
-                "Vous avez donnez un nombre incorrect d'arguments : %u au lieu de %u attendu.\n",
+                "Vous avez donné un nombre incorrect d'arguments : %u au lieu de %u attendu.\n",
                 actual,
                 expected);
         return false;
