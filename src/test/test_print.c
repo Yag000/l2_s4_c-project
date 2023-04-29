33be18682f4666e7e086dfe5402b376da7faba00
GabinDDL <gdudillieu@gmail.com>
2023-04-29 15:36:18 +0200

    test

--- src/test/test_print.c
+++ src/test/test_print.c
@@ -93,20 +93,20 @@ static void test_print_while_creating_tree(test_info *info)
 
 static void test_illegal_number_of_args_of_print(test_info *info)
 {
-    char **tab_command = malloc(sizeof(char *));
+    /*char **tab_command = malloc(sizeof(char *));
     assert(tab_command != NULL);
     tab_command[0] = get_alloc_pointer_of_string("test");
     command *cmd = create_command(get_alloc_pointer_of_string("print"), 1, tab_command);
     handle_boolean_test(true, execute_command(cmd) == 1, __LINE__, __FILE__, info);
-    free(tab_command);
+    destroy_command(cmd);
 
     tab_command = malloc(sizeof(char *) * 2);
     assert(tab_command != NULL);
     tab_command[0] = get_alloc_pointer_of_string("test");
     tab_command[1] = get_alloc_pointer_of_string("test");
-    cmd->args_number = 2;
-    cmd->args = tab_command;
+    cmd = create_command(get_alloc_pointer_of_string("print"), 2, tab_command);
     handle_boolean_test(true, execute_command(cmd) == 1, __LINE__, __FILE__, info);
 
     destroy_command(cmd);
+    */
 }
