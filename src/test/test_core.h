68938fd4f6410240a7d8042846dd963473ffa557
GabinDDL <gdudillieu@gmail.com>
2023-04-29 21:30:18 +0200

    Squashed commit of the following:
    commit 92e1a3f1c31bb6a9655116e2b25c58647b183b56
    Merge: 0df1a93 50db82a
    Author: Dudillieu Gabin <102906650+GabinDDL@users.noreply.github.com>
    Date:   Sat Apr 29 21:08:53 2023 +0200
    
        Merge pull request #66 from Yag000/14-Print
    
        14 print
    
    commit 50db82ab67f55d57b4014e273ded5c8a99357356
    Author: GabinDDL <gdudillieu@gmail.com>
    Date:   Sat Apr 29 20:43:05 2023 +0200
    
        Change number or errors
    
    commit fa1393cff1895c4d12ac6ac7d708a4e0bfe41dad
    Author: GabinDDL <gdudillieu@gmail.com>
    Date:   Sat Apr 29 20:38:48 2023 +0200
    
        Change print test message error
    
    commit a1c01eec8d5fe6ed602f25cbc6d742ef1e293473
    Merge: ae8058f 0df1a93
    Author: GabinDDL <gdudillieu@gmail.com>
    Date:   Sat Apr 29 20:37:40 2023 +0200
    
        Merge branch 'develop' into 14-Print
    
    commit ae8058ffb74e44510914382f62bece62b21e5cf6
    Author: GabinDDL <gdudillieu@gmail.com>
    Date:   Sat Apr 29 20:15:15 2023 +0200
    
        Update error message of expected output
    
    commit 504c3522e6cef7fd2b015ab08bbacecdfdd6ca91
    Author: Dudillieu Gabin <102906650+GabinDDL@users.noreply.github.com>
    Date:   Sat Apr 29 20:11:47 2023 +0200
    
        Update src/main/command.c
    
        Co-authored-by: Yag000 <80483460+Yag000@users.noreply.github.com>

--- src/test/test_core.h
+++ src/test/test_core.h
@@ -40,6 +40,8 @@ test_info *test_parser();
 test_info *test_command();
 test_info *test_pwd();
 test_info *test_print();
+test_info *test_pwd();
+test_info *test_print();
 test_info *test_rm();
 test_info *test_mkdir();
 
