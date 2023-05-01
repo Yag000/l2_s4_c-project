cee068eb93786a666ce68be754aee5957686eaca
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2023-05-01 10:48:20 +0200

    cd implementation

--- src/main/cd.c
+++ src/main/cd.c
@@ -1,31 +1,34 @@
 #include <stdlib.h>
 
 #include "command.h"
+#include "constants.h"
 #include "tree_dir_core.h"
 
-int cd(command *cmd)
+int cd(const command *cmd)
 {
-    // TODO handle to many arguments
+    if (!handle_number_of_args_with_delimitation(0, 1, cmd->args_number))
+    {
+        return INVALID_NUMBER_OF_ARGS;
+    }
 
 
     if (cmd->args_number == 0)
     {
         current_node = current_node->racine;
-        // TODO: update this with the appropriate constant from PR 55
-        return 0;
+        write_result_command("");
+        return SUCCESS;
     }
 
     noeud *tmp = search_node_in_tree(current_node, cmd->args[0]);
 
-    if (tmp == NULL)
+    if (tmp == NULL || !tmp-> est_dossier)
     {
         // TODO: improve this error message
-        write_result_command("Chemin non valide.");
-        // TODO: update this with the appropriate constant from PR 55
-        return 1;
+        write_result_command("Invalid path.");
+        return INVALID_PATH;
     }
 
     current_node = tmp;
-    // TODO: update this with the appropriate constant from PR 55
-    return 0;
+    write_result_command("");
+    return SUCCESS;
 }
