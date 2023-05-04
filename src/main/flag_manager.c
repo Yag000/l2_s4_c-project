1cb9d8e7cccc4ad76cb25bea1e002cd71883d764
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2023-05-04 20:31:10 +0200

    Refactorize the flag manager

---
+++ src/main/flag_manager.c
@@ -0,0 +1,89 @@
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "constants.h"
+#include "file_manager.h"
+#include "flag_manager.h"
+#include "string_utils.h"
+
+char *out_stream_path ;
+FILE *out_stream ;
+
+
+typedef struct flags
+{
+    bool verbose;
+    bool interactive;
+    bool error_occurs_stop;
+    char *output_file;
+} flags;
+
+static flags *parse_flags(int, char *[]);
+static void activate_flags(flags *);
+
+void initialize_flags(int argc, char *argv[])
+{
+    flags *flag = parse_flags(argc, argv);
+    activate_flags(flag);
+}
+
+static flags *parse_flags(int argc, char *argv[])
+{
+    flags *flag = malloc(sizeof(flags));
+    flag->verbose = false;
+    flag->interactive = false;
+    flag->error_occurs_stop = true;
+
+    if (strcmp(argv[1], "-i") == 0)
+    {
+        flag->interactive = true;
+    }
+
+    for (int i = 2; i < argc; i++)
+    {
+        if (strcmp(argv[i], "-v") == 0)
+        {
+            flag->verbose = true;
+            continue;
+        }
+        if (strcmp(argv[i], "-c") == 0)
+        {
+            flag->error_occurs_stop = false;
+            continue;
+        }
+        if (strcmp(argv[i], "-i") == 0)
+        {
+            // The -i flag is already handled but we want to allow duplicate flags
+            continue;
+        }
+        if (starts_with(argv[i], "-o="))
+        {
+            flag->output_file = argv[i] + 3;
+            continue;
+        }
+
+        perror("An unknown flag was given");
+        exit(EXIT_FAILURE);
+    }
+    return flag;
+}
+
+static void activate_flags(flags *flag)
+{
+    verbose = flag->verbose;
+    interactive = flag->interactive;
+    error_occurs_stop = flag->error_occurs_stop;
+
+    if (flag->output_file != NULL)
+    {
+        out_stream_path = flag->output_file;
+        out_stream = open_file(out_stream_path, "w");
+        if (out_stream == NULL)
+        {
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    free(flag);
+}
--- src/main/main.c
+++ src/main/main.c
@@ -3,28 +3,25 @@
 #include <string.h>
 
 #include "constants.h"
+#include "file_manager.h"
+#include "flag_manager.h"
 #include "parser.h"
+#include "string_utils.h"
 #include "tree_dir_core.h"
 
+char *out_stram_path;
 FILE *out_stream;
 
 bool verbose = false;
+
 noeud *current_node;
-
-typedef struct flags
-{
-    bool verbose;
-    bool interactive;
-    bool error_occurs_stop;
-} flags;
-
-static flags *parse_flags(int, char *[]);
-static void activate_flags(flags *);
 
 int main(int argc, char *argv[])
 {
-    out_stream = stdout;
 
+    out_stream = stdout;
+    out_stram_path = NULL;
+ 
     // Hnadle 0 arguments
     if (argc <= 1)
     {
@@ -32,8 +29,7 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
-    flags *flag = parse_flags(argc, argv);
-    activate_flags(flag);
+    initialize_flags(argc, argv);
 
     current_node = create_root_noeud();
 
@@ -42,49 +38,4 @@ int main(int argc, char *argv[])
     destroy_tree();
 
     return error_code == SUCCESS || error_code == EXIT_PROGRAM_SUCCESS ? EXIT_SUCCESS : EXIT_FAILURE;
-}
-
-static flags *parse_flags(int argc, char *argv[])
-{
-    flags *flag = malloc(sizeof(flags));
-    flag->verbose = false;
-    flag->interactive = false;
-    flag->error_occurs_stop = true;
-
-    if (strcmp(argv[1], "-i") == 0)
-    {
-        flag->interactive = true;
-    }
-
-    for (int i = 2; i < argc; i++)
-    {
-        if (strcmp(argv[i], "-v") == 0)
-        {
-            flag->verbose = true;
-            continue;
-        }
-        if (strcmp(argv[i], "-c") == 0)
-        {
-            flag->error_occurs_stop = false;
-            continue;
-        }
-        if (strcmp(argv[i], "-i") == 0)
-        {
-            // The -i flag is already handled but we want to allow duplicate flags
-            continue;
-        }
-
-        perror("An unknown flag was given");
-        exit(EXIT_FAILURE);
-    }
-    return flag;
-}
-
-static void activate_flags(flags *flag)
-{
-    verbose = flag->verbose;
-    interactive = flag->interactive;
-    error_occurs_stop = flag->error_occurs_stop;
-
-    free(flag);
 }
