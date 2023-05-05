b8018b47edf711503e89a18b85ac8b1b38ba1dd1
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2023-05-05 13:17:09 +0200

    First implementation of the valgrind flag

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 has_passed=true
+use_valgrind=false
+verbose=false
 output_dir="src/test/output"
 
 COLOR_OFF="$(tput sgr0)"
@@ -8,6 +10,7 @@ RED="$(tput setaf 1)"
 GREEN="$(tput setaf 2)"
 
 TEMP_DIFF_FILE=".diff_tmp"
+TEMP_VALGRIND_FILE=".valgrind_tmp"
 
 
 function clean(){
@@ -21,7 +24,7 @@ function test_one_output(){
     local output_dir=$2
     local file=$3
     local output_file="$output_dir/$file"
-    
+
     if  ! diff -y  "$expected_output_dir/$file" "$output_file" > $TEMP_DIFF_FILE;
     then
         printf "%s%s%s\n" $RED "FAIL: $file" $COLOR_OFF
@@ -32,20 +35,37 @@ function test_one_output(){
     return 0
 }
 
+function run_with_valgrind(){
+    local executable=$1
+    shift
+    local input_file=$1
+    shift
+    local flags=("$@")
+    echo "Running valgrind on $executable $input_file ${flags[@]}"
+    if ! valgrind --leak-check=full --show-leak-kinds=all --errors-for-leak-kinds=all --error-exitcode=1 --log-file="$TEMP_VALGRIND_FILE" -q $executable  $input_file "${flags[@]}" ;
+    then
+        printf "%s%s%s\n" $RED "Valgrind FAIL: $input_file" $COLOR_OFF
+        cat $TEMP_VALGRIND_FILE
+        echo "----------------------------------------------"
+        return 1
+    fi
+    return 0
+}
+
 function test_main(){
     echo
     echo "|-=-=-=-=-=-=-=-=-| Testing compiling main |-=-=-=-=-=-=-=-=-|"
     make all
-    
+
     echo
     echo "|-=-=-=-=-=-=-=-=-| Testing main function |-=-=-=-=-=-=-=-=-|"
-    
+
     local has_main_test_failed=false
     local expected_output_dir="src/test/test_main/expected_output"
     local output_dir="src/test/test_main/output"
     local input_dir="src/test/test_main/input"
     [ ! -d "$output_dir" ] && mkdir "$output_dir"
- 
+
     # Get a list of output file names (excluding directories)
     local expected_output_files=$(find $expected_output_dir -type f -name "*.txt" -printf "%f\n")
     # Loop through the input files
@@ -53,19 +73,25 @@ function test_main(){
         # Create the corresponding output file name
         local output_file="$output_dir/$file"
         local input_file="$input_dir/$file"
-        local flags=""
-        
-        echo $file | grep "verbose.txt$" > /dev/null && flags="-v"
-        ./main  "$input_file"  $flags > "$output_file"
-        
+        local flags=()
+
+        flags+=("-o=$output_file")
+        echo $file | grep "verbose" > /dev/null && flags+=("-v")
+
+        if $use_valgrind; then
+            run_with_valgrind "./main" $input_file  "${flags[@]}" || has_main_test_failed=true
+        else
+            ./main $input_file "${flags[@]}" || has_main_test_failed=true
+        fi
+
         # if the test fails then set has_test_output_failed to true
         test_one_output $expected_output_dir $output_dir $file || has_main_test_failed=true
     done
-    
+
     rm $TEMP_DIFF_FILE
     $has_main_test_failed && has_passed=false
     ($has_main_test_failed && printf "%s%s%s\n" $RED "There is at least one problem with the main function" $COLOR_OFF ) || printf '%s%s%s\n' $GREEN "The main function works" $COLOR_OFF
-    
+
 }
 
 function compile_test(){
@@ -76,32 +102,38 @@ function compile_test(){
 
 function run_tests(){
     echo
-    echo "|-=-=-=-=-=-=-=-=-| Running tests |-=-=-=-=-=-=-=-=-|"
-    
-    if ./test $1; then
-        has_passed=true
+
+    local flag=""
+    $verbose && flag="-v"
+
+    if $valgrind ; then
+        echo "|-=-=-=-=-=-=-=-=-| Running tests with valgrind |-=-=-=-=-=-=-=-=-|"
+        run_with_valgrind   "./test" "" $flag || has_passed=false
     else
-        has_passed=false
+        echo "|-=-=-=-=-=-=-=-=-| Running tests |-=-=-=-=-=-=-=-=-|"
+        ./test $flag || has_passed=false
     fi
+
+    rm $VALGRIND_TMP_FILE
 }
 
 function test_ouput(){
     echo
     echo "|-=-=-=-=-=-=-=-=-| Testing output |-=-=-=-=-=-=-=-=-|"
-    
+
     local has_test_output_failed=false
     local expected_output_dir="src/test/expected_output"
     local output_dir="src/test/output"
-    
+
     # Get a list of input file names (excluding directories)
     local expected_output_files=$(find $expected_output_dir -type f -name "*.txt" -printf "%f\n")
-    
+
     # Loop through the input files
     for file in $expected_output_files; do
         # if the test fails then set has_test_output_failed to true
         test_one_output $expected_output_dir $output_dir $file || has_test_output_failed=true
     done
-    
+
     rm $TEMP_DIFF_FILE
     $has_test_output_failed && has_passed=false
     ($has_test_output_failed && printf "%s%s%s\n" $RED "There is at least one difference from the expected output" $COLOR_OFF ) || printf '%s%s%s\n' $GREEN "The output is correct" $COLOR_OFF
@@ -109,6 +141,24 @@ function test_ouput(){
 
 
 [ ! -d "$output_dir" ] && mkdir "$output_dir"
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -v)
+            echo "Verbose mode"
+            verbose=true
+            ;;
+        -valgrind)
+            echo "Using valgrind"
+            use_valgrind=true
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+    esac
+    shift
+done
 
 
 # Clean up before running the tests
@@ -120,7 +170,8 @@ test_main
 # Compile and run the tests
 compile_test
 
-run_tests $1
+# Run tests
+run_tests
 
 # Test the output
 test_ouput
@@ -134,7 +185,3 @@ $has_passed && exit 0
 
 # Exit with a zero status if all the tests passed
 exit 1
-
-    
-    
-
