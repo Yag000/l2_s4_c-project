cd7f6b432b631bc75308fb2e848005635a3af1ee
GabinDDL <gdudillieu@gmail.com>
2023-05-06 16:01:05 +0200

    Translation

--- src/main/parser.c
+++ src/main/parser.c
@@ -119,7 +119,7 @@ int parse_line(char *line)
 
     if (iterator == NULL)
     {
-        perror("Problème initialisation iterator");
+        perror("Initialization iterator problem");
 
         free(line);
         return FATAL_ERROR;
@@ -129,7 +129,7 @@ int parse_line(char *line)
 
     if (command == NULL)
     {
-        perror("Problème creation commande");
+        perror("Command creation problem");
 
         free(line);
         destroy_string_iterator(iterator);
@@ -152,7 +152,7 @@ static command *get_command_from_iterator(string_iterator *iterator)
 {
     if (!has_next_word(iterator))
     {
-        perror("Problème iterator vide");
+        perror("Empty iterator problem");
         return NULL;
     }
 
